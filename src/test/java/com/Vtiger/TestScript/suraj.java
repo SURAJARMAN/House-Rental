dc080a372dcd59bdd94513016e91d03eec241bc9
Mdyusufb <Mdyusufb95@gmail.com>
2022-11-22 10:35:13 +0530

    updated pls check it

--- src/test/java/com/Vtiger/TestScript/suraj.java
+++ src/test/java/com/Vtiger/TestScript/suraj.java
@@ -4,7 +4,7 @@ package com.Vtiger.TestScript;
 
 public class suraj {
 	public static void main(String[] args) {
-		System.out.println("babab saheb");
+		System.out.println("baba saheb");
 		System.out.println("babab saheb");
 		System.out.println("babab saheb");
 		System.out.println("babab saheb");
